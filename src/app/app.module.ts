cce5493270b64eea642909e3d28ae80d0d25bfed
ld <christian.beneteau@lepoles.com>
2020-02-04 12:16:36 +0100

    etape3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { AdditionComponent } from './addition/addition.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    AdditionComponent
+    AdditionComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero [] = [
+
+    { id: 1, name: "Superman"},
+    { id: 2, name: "Batman" },
+    { id: 3, name: "Ligthwin" },
+    { id: 4, name: "Robin" },
+    { id: 5, name: "Wonder woman" },
+    { id: 6, name: "Flash" },
+    { id: 7, name: "Joker" },
+    { id: 8, name: "alien" },
+    { id: 9, name: "Super Kiki" },
+    { id: 10, name: "Super Boy" },
+];
